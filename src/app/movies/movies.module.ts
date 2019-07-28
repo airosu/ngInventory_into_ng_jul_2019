30fa278f265d79ac7c907bcc0f39b2f213d09fa6
Andrei Antal <antal.andrei@icloud.com>
2019-07-28 19:20:56 +0300

    Challenge_4

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material.module';
+
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [MovieItemComponent],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
   exports: [MovieItemComponent]
 })
 export class MoviesModule { }
