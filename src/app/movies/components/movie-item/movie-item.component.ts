2c5919a66260e187303ff3d0078ed7fd4309c982
Andrei Antal <antal.andrei@icloud.com>
2019-07-28 19:15:10 +0300

    Challenge_2

---
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngi-movie-item',
+  templateUrl: './movie-item.component.html',
+  styleUrls: ['./movie-item.component.css']
+})
+export class MovieItemComponent { }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+
 @NgModule({
+  declarations: [MovieItemComponent],
   imports: [ CommonModule ],
+  exports: [MovieItemComponent]
 })
 export class MoviesModule { }
